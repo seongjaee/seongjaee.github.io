24612739f4f6a7ebfa355d197aebcdc52c588d29
seongjaee <labyrinthird@gmail.com>
2021-11-28 18:03:24 +0900

    Add random AI

--- simple_games/tic-tac-toe copy.js
+++ simple_games/tic-tac-toe copy.js
@@ -19,17 +19,17 @@ function init() {
   })
 }
 
-function onSelect(event) {
-  if (!event.target.innerText) {
+function onSelect(td) {
+  if (!td.innerText) {
     if (turn === 0) {
       turn = 1
-      event.target.innerText = 'O'
-      selectedOs.push(event.target.id)
+      td.innerText = 'O'
+      selectedOs.push(td.id)
       h2Tag.innerText = "X 's turn"
     } else {
       turn = 0
-      event.target.innerText = 'X'
-      selectedXs.push(event.target.id)
+      td.innerText = 'X'
+      selectedXs.push(td.id)
       h2Tag.innerText = "O 's turn"
     }
   }
@@ -56,28 +56,66 @@ function isOver() {
   return selectedOs.length + selectedXs.length === 9
 }
 
-function onClick(event) {
-  onSelect(event)
+function checkGame() {
   if (isWinner(selectedOs)){
     setTimeout(function() {
       alert('O Win !')
       init()
     }, 150)
-    
+    return true
   } else if (isWinner(selectedXs)) {
     setTimeout(function() {
       alert('X Win !')
       init()
     }, 150)
+    return true
   } else if (isOver()) {
     setTimeout(function() {
       alert('Draw !')
       init()
     }, 150)
+    return true
   }
+  return false
 }
 
+// turn = 0일 때, O의 차례에만 플레이어가 선택할 수 있도록
+function onClick(td) {
+  if (td.innerText || turn === 1) {
+    return
+  }
+  onSelect(td)
+  if (checkGame()) {
+    return
+  }
+  setTimeout(function () {
+    comSelect()
+    if (checkGame()) {
+      return
+    }
+  }, 600)
+}
+
+function comRandomSelect() {
+  let num = Math.floor(Math.random() * 9)
+  let tdId = `_${parseInt(num/3)}${num%3}`
+  while (selectedOs.includes(tdId) || selectedXs.includes(tdId)) {
+    num = Math.floor(Math.random() * 9)
+    tdId = `_${parseInt(num/3)}${num%3}`
+  }
+  const td = document.getElementById(tdId) 
+  console.log('COM!', td)
+  onSelect(td)
+}
+
+// MIN-MAX ALGORITHM
+function comSmartSelect() {
+  
+}
+
+
+
 cells.forEach(cell => {
-  cell.addEventListener('click', event => onClick(event))
+  cell.addEventListener('click', event => onClick(event.target))
 })
 

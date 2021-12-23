563171d5329565ad6ff7dd6aed0cc2c109ab907c
seongjaee <labyrinthird@gmail.com>
2021-12-23 16:03:54 +0900

    Refact

--- simple_games/15-puzzle.js
+++ simple_games/15-puzzle.js
@@ -117,36 +117,44 @@ blocks.forEach(block => {
 })
 
 // 키보드 입력에 따른 동작
-document.addEventListener('keydown', function(event) {
-  switch (event.code) {
-    case 'ArrowUp': {
-      if (blankPoint[0] < 3){
-        setBlank(blankPoint[0] + 1, blankPoint[1])
-      }
-      break
-    }
-    case 'ArrowDown': {
-      if (blankPoint[0] > 0){
-        setBlank(blankPoint[0] - 1, blankPoint[1])
-      }
-      break
-    }
-    case 'ArrowLeft': {
-      if (blankPoint[1] < 3){
-        setBlank(blankPoint[0], blankPoint[1] + 1)
-      }
-      break
-    }
-    case 'ArrowRight': {
-      if (blankPoint[1] > 0){
-        setBlank(blankPoint[0], blankPoint[1] - 1)
-      }
-      break
-    }
-    case 'Space':{
-      shuffle()
-    }
+function moveUp() {
+  if (blankPoint[0] < 3){
+    setBlank(blankPoint[0] + 1, blankPoint[1])
   }
+}
+
+function moveDown() {
+  if (blankPoint[0] > 0){
+    setBlank(blankPoint[0] - 1, blankPoint[1])
+  }
+}
+
+function moveLeft() {
+  if (blankPoint[1] < 3){
+    setBlank(blankPoint[0], blankPoint[1] + 1)
+  }
+}
+
+function moveRight() {
+  if (blankPoint[1] > 0){
+    setBlank(blankPoint[0], blankPoint[1] - 1)
+  }
+}
+
+const funcs = {
+  ArrowUp: moveUp,
+  ArrowDown: moveDown,
+  ArrowLeft: moveLeft,
+  ArrowRight: moveRight,
+  Space: shuffle,
+}
+
+document.addEventListener('keydown', function(event) {
+  key = event.code
+  if (key in funcs) {
+    funcs[key]()
+  }
+
   if (isCleared()){
     setTimeout(function() {
       confirm('Clear!')
